53f4f7b4d681ba44dedacd11f6c0b9c55750b982
Alexander Davis <akonidavis@gmail.com>
2017-03-14 19:47:54 -1000

    Choose a Number!

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,10 @@
-'use strict';
-module.exports = function() {
-  
-};
+"use strict";
+module.exports = (function secretNumberGenerator() {
+	var number = Math.floor((Math.random() * 1000000));
+
+	function secretNumber(){
+		return number;
+	};
+
+	return secretNumber;
+});
